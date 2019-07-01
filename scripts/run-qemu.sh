2413ff1cae5211f322d2cebf54d93e7f956c08bc
Dayeol Lee <dayeol@berkeley.edu>
2019-07-01 17:58:04 +0000

    Linux + SM boots in RV32

--- scripts/run-qemu.sh
+++ scripts/run-qemu.sh
@@ -13,12 +13,12 @@ if [ "$1" = "-debug" ]; then
 fi
 done
 
+#./qemu/riscv32-softmmu/qemu-system-riscv32 \
 ./riscv-qemu/riscv32-softmmu/qemu-system-riscv32 \
     $DEBUG \
-    -m 4G \
+    -bios bootrom/bootrom.elf \
     -nographic \
     -machine virt \
     -kernel hifive-work/riscv-pk/bbl \
     -netdev user,id=net0,net=192.168.100.1/24,dhcpstart=192.168.100.128,hostfwd=tcp::${HOST_PORT}-:22 \
     -device virtio-net-device,netdev=net0
-    #-bios bootrom/bootrom.elf \
