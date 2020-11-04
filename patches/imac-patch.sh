e64932a2732c6926be6e739355a77612dce985aa
thuchoang90 <thuc@vlsilab.ee.uec.ac.jp>
2020-11-04 21:08:33 +0900

    Recorrect rv64imac patches for the new sdk

--- patches/imac-patch.sh
+++ patches/imac-patch.sh
@@ -1,7 +1,5 @@
 patch -p1 < patches/conf-imac.patch
 cd riscv-pk/
 patch -p1 < ../patches/riscv-pk-qemu-misa.patch
-cd ../sdk/rts/eyrie/
-patch -p1 < ../../../patches/sdk-imac.patch
-cd ../../../
+cd ../
 
